33811df9b0f82dd1c8c6d6cd394fe9d31ae8e788
necritchfield <127362771+necritchfield@users.noreply.github.com>
2023-05-24 17:35:02 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require('express');
 const app = express();
 const PORT = 9036;
 
+app.use(express.json());
 app.use(express.static('public'));
 
 app.use(express.urlencoded({
@@ -27,6 +28,21 @@ const db = require('./database/db-connector');
 
 /* operations for: Readers entity */
 // CREATE
+app.post('/add-reader', (req, res) => {
+    let data = req.body;
+    let sql = `INSERT INTO Readers (name, email) VALUES ('${data.name}', '${data.email}')`;
+    db.pool.query(sql, (error, rows, field) => {
+	if (error) {
+	    console.log(error);
+	    res.sendStatus(400);
+	}
+	else {
+	    res.redirect('/');
+	}
+    })
+});
+
+
 // RETRIEVE
 app.get('/retrieve-readers', (req, res) => {
     let sql = 'SELECT * FROM Readers;';
@@ -37,6 +53,18 @@ app.get('/retrieve-readers', (req, res) => {
 });
 // UPDATE
 // DELETE
+app.delete('/delete-reader', function(req, res, next) {
+    let data = req.body;
+    let readerID = parseInt(data.id);
+    let sql = `DELETE FROM Readers WHERE readerID = ?`;
+
+    db.pool.query(sql, [readerID], function(error, rows, fields) {
+	if (error) {
+	    console.log(error);
+	    res.sendStatus(400);
+	}
+    })
+});
 
 /* operations for: ReadingClubs entity */
 /* operations for: Books entity */
